f094fe7e5ba4711c9d80eb52f4cc76d000d70bc1
amandadzunuri <amandadzunuri@gmail.com>
2024-05-29 15:09:49 +0700

    add splash screen

---
+++ lib/ui/splash/splashscreen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:kalih_coffee/ui/pelanggan/daftar_menu.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToMenu();
+  }
+
+  _navigateToMenu() async {
+    await Future.delayed(Duration(milliseconds: 3000), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => DaftarMenu()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:
+          Color(0xFFF5F5E8), // Matches the background color in the image
+      body: Center(
+        child: Image.asset('assets/images/logo_kalih.png'),
+      ),
+    );
+  }
+}
